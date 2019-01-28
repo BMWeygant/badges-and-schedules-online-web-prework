149f213320e13a7d9ba71ecefbc6a4082e71a7ac
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-01-28 03:12:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 def badge_maker(name)
  "Hello, my name is #{name}."
 end
-def batch_badge_creator(array)
+def batch_badge_creator(attendees)
 attendees.map ={ |attendee|
 "Hello, my name is #{attendee}".}
 
