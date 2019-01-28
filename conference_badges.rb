ee3c09bcd854c6852ca1c34451d261fc2ff1f678
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-01-28 02:59:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+def batch_badge_creator(name)
+array = guest_list
+"Hello, my name is #{name}"
+guest_list += 1
+
+end
